bdece12c9ae50994721b4ebf15e104f1c64e3308
Nastya <125801230+AnastasiaMudrik@users.noreply.github.com>
2023-03-11 22:56:40 +0300

    Add files via upload

---
+++ vsc/main.js
@@ -0,0 +1,9 @@
+var name = prompt('Enter your name:');
+console.log(name);
+alert(name);
+var surname = prompt('Enter your surname:');
+console.log(surname);
+alert(surname);
+var age = prompt('Enter your age:');
+console.log(age);
+alert(age);
